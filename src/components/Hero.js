b8b1fcbf51bc6dad294f84fa6559e282ca765bdc
avoajaugochukwu <karlthegoo@hotmail.com>
2021-08-23 16:21:48 +0100

    Updated padding for Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <div className=" lg:px-20 md:px-8 px-8 ">
       <div className="hero-background md:py-40 py-32 md:pl-20 text-white text-center md:text-left">
-        <h1 className="text-7xl pb-5 font-black">Create Plan</h1>
+        <h1 className="text-7xl px-8 pb-5 font-black">Create Plan</h1>
         <p className="md:w-1/3 px-8">
           Coffee the way you wanted it to be. For coffee delivered tomorrow, or next week. 
           For whatever brew method you use. For choice, for convenience, for quality.
